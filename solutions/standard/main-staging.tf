fb6191e6111c9afd62b0a7af965049c9a07b86cc
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-23 14:44:15 -0500

    add do nothing terraform for ys1-staging testing only

---
+++ solutions/standard/main-staging.tf
@@ -0,0 +1,62 @@
+resource "time_sleep" "wait_9_seconds" {
+    create_duration = "9s"
+}
+##############################################################################
+
+output "watsonx_ai_runtime_crn" {
+    value       = "watsonx_ai_runtime_crn"
+}
+
+output "watsonx_ai_runtime_guid" {
+    value       = "watsonx_ai_runtime_guid"
+}
+
+output "watsonx_ai_runtime_name" {
+    value       = "watsonx_ai_runtime_name"
+}
+
+output "watsonx_ai_runtime_plan_id" {
+    value       = "watsonx_ai_runtime_plan_id"
+}
+
+output "watsonx_ai_runtime_dashboard_url" {
+    value       = "watsonx_ai_runtime_dashboard_url"
+}
+
+output "watsonx_ai_runtime_account_id" {
+    value       = "watsonx_ai_runtime_account_id"
+}
+
+# watsonx.ai Studio
+output "watsonx_ai_studio_crn" {
+    value       = "watsonx_ai_studio_crn"
+}
+
+output "watsonx_ai_studio_guid" {
+    value       = "watsonx_ai_studio_guid"
+}
+
+output "watsonx_ai_studio_name" {
+    value       = "watsonx_ai_studio_name"
+}
+
+output "watsonx_ai_studio_plan_id" {
+    value       = "watsonx_ai_studio_plan_id"
+}
+
+output "watsonx_ai_studio_dashboard_url" {
+    value       = "watsonx_ai_studio_dashboard_url"
+}
+
+# watsonx.ai Project
+output "watsonx_ai_project_id" {
+    value       = "watsonx_ai_project_id"
+}
+
+output "watsonx_ai_project_bucket_name" {
+    value       = "watsonx_ai_project_bucket_name"
+}
+
+output "watsonx_ai_project_url" {
+    value       = "watsonx_ai_project_url"
+}
